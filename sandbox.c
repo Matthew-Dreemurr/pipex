c91de5c1915cbd7b1b0584d1c65b2894bac55e04
Matthew Hadad <mahadad@fu-r6-p2.s19.be>
2022-03-13 16:29:30 +0100

    Makefile push 2022-03-13 16:29:30

--- sandbox.c
+++ sandbox.c
@@ -6,7 +6,7 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 13:27:06 by mahadad           #+#    #+#             */
-/*   Updated: 2022/03/13 15:15:36 by mahadad          ###   ########.fr       */
+/*   Updated: 2022/03/13 16:28:28 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,42 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
 
 int
 main(int argc, char *argv[])
 {
-	pid_t	cpid;
-	cpid = fork();
-	if (cpid)
+	setbuf(stdout, NULL);
+
+	char buff[500];
+	while (1)//Test stdin
 	{
-		printf("Parent %d\n", cpid);
+		printf("Read fd 0 [%lu]\n", read(STDIN_FILENO, buff, 1));
+		if (*buff == 'q')
+			break;
 	}
-	if (!cpid){
-		printf("Child %d\n", cpid);
-		cpid = fork();
-		if (cpid)
-			printf("Child of child %d\n", cpid);
+
+	int oldStdou = dup(STDIN_FILENO);//bkp stdin
+	printf("oldStdin = %d\n", oldStdou);
+	close(STDIN_FILENO);//Close stdin
+
+	printf("Try to read old stdin fd [%zd]\n", read(STDIN_FILENO, buff, 1));//Check if stdout is not readeble
+	
+
+	dup2(oldStdou, 5);
+	
+	while (1)//Test read stdin in fd 5
+	{
+		printf("Read fd 5 [%lu]\n", read(5, buff, 1));
+		if (*buff == 'q')
+			break;
 	}
+	
+	int	newFile;
+	if (0 > (newFile = open("./Makefile", O_RDONLY)))
+		return (1);
+	dup2(newFile, STDIN_FILENO);
+	read(STDIN_FILENO, buff, 449);
+	printf("Write buff for stdin:\n%s", buff);
+	return (0);
 }
