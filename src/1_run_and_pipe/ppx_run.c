37851d5aa63968d4d69e21fefbb23086ed37bab4
Matthew Hadad <mahadad@fu-r6-p2.s19.be>
2022-03-15 17:34:40 +0100

    Makefile push 2022-03-15 17:34:40

--- src/1_run_and_pipe/ppx_run.c
+++ src/1_run_and_pipe/ppx_run.c
@@ -6,7 +6,7 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 14:50:21 by mahadad           #+#    #+#             */
-/*   Updated: 2022/03/15 16:55:18 by mahadad          ###   ########.fr       */
+/*   Updated: 2022/03/15 17:34:34 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,11 @@ static void	ppx_open_files(t_data *data)
 }
 /**
 **/
+#include <stdio.h> //TODO REMOVE
 static void	ppx_middle_cmd(t_data *data, char **env, int fds[])
 {
 	int	frk;
-
+	printf("PParent : %d,%d\n", fds[0], fds[1]);
 	frk = fork();
 	if (frk > 0)
 	{
@@ -45,6 +46,8 @@ static void	ppx_middle_cmd(t_data *data, char **env, int fds[])
 	}
 	else if (!frk)
 	{
+		printf("CChild : %d,%d\n", fds[0], fds[1]);
+		close(fds[1]);
 		dup2(fds[0], STDIN_FILENO);
 		dup2(data->out_file, STDOUT_FILENO);
 		execve(data->cmd[1].bin, data->cmd[1].arg, env);
@@ -52,15 +55,13 @@ static void	ppx_middle_cmd(t_data *data, char **env, int fds[])
 	
 }
 
-#include <stdio.h> //TODO REMOVE
 void	ppx_run(t_data *data, char **env)
 {
 	int	frk;
 	int	fds[2];
-	int	old_stdout;
 
-	old_stdout = dup(STDOUT_FILENO);
 	ppx_open_files(data);
+	data->old_stdout = dup(STDOUT_FILENO);
 	if (pipe(fds))
 		ppx_exit_prog(EXIT_FAILURE, data, NULL);
 	printf("Parent : %d,%d\n", fds[0], fds[1]);
@@ -68,6 +69,8 @@ void	ppx_run(t_data *data, char **env)
 	if (frk > 0)//end
 	{
 		ppx_middle_cmd(data, env, fds);
+		printf("end_middle_cmd\n");
+		dup2(STDOUT_FILENO, STDOUT_FILENO);
 		close(data->in_file);
 		close(data->out_file);
 		close(fds[0]);
@@ -76,11 +79,10 @@ void	ppx_run(t_data *data, char **env)
 	}
 	else if (!frk)//exe fist cmd and pipe
 	{
+		printf("Child : %d,%d\n", fds[0], fds[1]);
 		dup2(data->in_file, STDIN_FILENO);
 		dup2(fds[1], STDOUT_FILENO);
 		execve(data->cmd[0].bin, data->cmd[0].arg, env);
-		dup2(STDOUT_FILENO, old_stdout);
-		printf("Child : %d,%d\n", fds[0], fds[1]);
 	}
 	else
 		;//TODO return error
--- src/utils/ppx_struct.h
+++ src/utils/ppx_struct.h
@@ -6,7 +6,7 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 11:22:26 by mahadad           #+#    #+#             */
-/*   Updated: 2022/03/15 14:52:26 by mahadad          ###   ########.fr       */
+/*   Updated: 2022/03/15 17:17:56 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ typedef struct s_data
 	char		**bin_dir;
 	int			in_file;
 	int			out_file;
+	int			old_stdout;
 	t_vector	vec;
 }				t_data;
 
